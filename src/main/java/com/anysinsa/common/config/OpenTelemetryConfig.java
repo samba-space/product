86582168df44ebb8a450aef638b17a197b85abe8
Park <junkipark430@gmai.com>
2024-11-13 18:07:17 +0900

    test telemetry

--- src/main/java/com/anysinsa/common/config/OpenTelemetryConfig.java
+++ src/main/java/com/anysinsa/common/config/OpenTelemetryConfig.java
@@ -1,29 +1,17 @@
 package com.anysinsa.common.config;
 
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenTelemetryConfig {
-//    @Bean
-//    public OpenTelemetry openTelemetry() {
-//        Resource resource = Resource.getDefault()
-//                .merge(Resource.create(Attributes.builder()
-//                        .put(ResourceAttributes.SERVICE_NAME, "product-service")
-//                        .put(ResourceAttributes.DEPLOYMENT_ENVIRONMENT, "production")
-//                        .build()));
-//
-//        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-//                .setResource(resource)
-//                .addSpanProcessor(BatchSpanProcessor.builder(
-//                                OtlpGrpcSpanExporter.builder()
-//                                        .setEndpoint("http://localhost:4317")
-//                                        .build())
-//                        .build())
-//                .build();
-//
-//        return OpenTelemetrySdk.builder()
-//                .setTracerProvider(sdkTracerProvider)
-//                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-//                .buildAndRegisterGlobal();
-//    }
+
+    @Bean
+    public OtlpGrpcSpanExporter otlpGrpcSpanExporter(@Value("${management.otlp.tracing.endpoint}") String endpoint) {
+        return OtlpGrpcSpanExporter.builder()
+                .setEndpoint(endpoint)
+                .build();
+    }
 }
