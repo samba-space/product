ee4f371ca170479101e79a8997930e339d133fe4
Park <junkipark430@gmai.com>
2024-11-13 12:21:25 +0900

    alb url modify

--- src/main/java/com/anysinsa/product/client/CategoryClient.java
+++ src/main/java/com/anysinsa/product/client/CategoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "category-service", url = "http://anysinsa-alb-1420791660.ap-northeast-2.elb.amazonaws.com")
+@FeignClient(name = "category-service", url = "http://anysinsa-alb-1164360136.ap-northeast-2.elb.amazonaws.com")
 public interface CategoryClient {
     @GetMapping("/api/categories/{categoryId}")
     CategoryResponseDTO findCategory(@PathVariable Long categoryId);
