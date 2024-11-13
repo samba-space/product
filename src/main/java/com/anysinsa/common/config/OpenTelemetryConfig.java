1386598dd5f5b10182592ac4e29213f676584fdb
Park <junkipark430@gmai.com>
2024-11-13 15:58:06 +0900

    modify

--- src/main/java/com/anysinsa/common/config/OpenTelemetryConfig.java
+++ src/main/java/com/anysinsa/common/config/OpenTelemetryConfig.java
@@ -1,40 +1,29 @@
 package com.anysinsa.common.config;
 
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-import io.opentelemetry.semconv.ResourceAttributes;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenTelemetryConfig {
-    @Bean
-    public OpenTelemetry openTelemetry() {
-        Resource resource = Resource.getDefault()
-                .merge(Resource.create(Attributes.builder()
-                        .put(ResourceAttributes.SERVICE_NAME, "product-service")
-                        .put(ResourceAttributes.DEPLOYMENT_ENVIRONMENT, "production")
-                        .build()));
-
-        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-                .setResource(resource)
-                .addSpanProcessor(BatchSpanProcessor.builder(
-                                OtlpGrpcSpanExporter.builder()
-                                        .setEndpoint("http://localhost:4317")
-                                        .build())
-                        .build())
-                .build();
-
-        return OpenTelemetrySdk.builder()
-                .setTracerProvider(sdkTracerProvider)
-                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-                .buildAndRegisterGlobal();
-    }
+//    @Bean
+//    public OpenTelemetry openTelemetry() {
+//        Resource resource = Resource.getDefault()
+//                .merge(Resource.create(Attributes.builder()
+//                        .put(ResourceAttributes.SERVICE_NAME, "product-service")
+//                        .put(ResourceAttributes.DEPLOYMENT_ENVIRONMENT, "production")
+//                        .build()));
+//
+//        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+//                .setResource(resource)
+//                .addSpanProcessor(BatchSpanProcessor.builder(
+//                                OtlpGrpcSpanExporter.builder()
+//                                        .setEndpoint("http://localhost:4317")
+//                                        .build())
+//                        .build())
+//                .build();
+//
+//        return OpenTelemetrySdk.builder()
+//                .setTracerProvider(sdkTracerProvider)
+//                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+//                .buildAndRegisterGlobal();
+//    }
 }
