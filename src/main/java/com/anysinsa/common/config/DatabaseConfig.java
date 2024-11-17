2b0204c9649e7da4e3fa48eec75502232e53078f
Park <junkipark430@gmai.com>
2024-11-18 00:07:02 +0900

    test

--- src/main/java/com/anysinsa/common/config/DatabaseConfig.java
+++ src/main/java/com/anysinsa/common/config/DatabaseConfig.java
@@ -42,11 +42,9 @@ public class DatabaseConfig {
         config.addDataSourceProperty("opentelemetry.jdbc.wrapped-driver", "com.mysql.cj.jdbc.Driver");
 
         // 복제 관련 설정(local)
-        config.addDataSourceProperty("readReplicas", "1");
         config.addDataSourceProperty("replicationEnableJMX", "true");
         config.addDataSourceProperty("allowMasterDownConnections", "true");
         config.addDataSourceProperty("allowReplicaDownConnections", "true");
-//        config.addDataSourceProperty("enableClusterAwareFailover", "true");
 
         return new HikariDataSource(config);
     }
